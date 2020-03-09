ac298349aff63f4de144a44c5c6703ff964ed357
alexmatusg <alex.matusg@gmail.com>
2020-03-09 12:26:31 -0300

    Java n1

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        final int edad = 25;
+        System.out.println("Mi nombre es Alex");
+        System.out.println("Tengo " + edad + " años de edad");
+
+    }
+}
